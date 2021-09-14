c07f241312943ad653f594cb652a4549d9231db0
Eric Torunski <etorunski@gmail.com>
2021-09-14 15:53:50 -0400

    Initial commit

---
+++ app/src/main/java/com/cst2335/torunse/MainActivity.java
@@ -0,0 +1,17 @@
+package com.cst2335.torunse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override   //public  static void main(String args[])
+    //this is our starting point
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //calls parent onCreate()
+
+        setContentView( R.layout.activity_main ); //loads XML on screen
+    }
+}
