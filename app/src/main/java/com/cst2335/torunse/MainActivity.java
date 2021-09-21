7b7697bf8db2bb82cb9d219021f7280270c603f1
Eric Torunski <etorunski@gmail.com>
2021-09-21 15:24:23 -0400

    working on text widgets

--- app/src/main/java/com/cst2335/torunse/MainActivity.java
+++ app/src/main/java/com/cst2335/torunse/MainActivity.java
@@ -2,6 +2,11 @@ package com.cst2335.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,35 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calls parent onCreate()
+        //nothing is loaded
+//        findViewById()//always fail here
+
+
         setContentView( R.layout.activity_main ); //loads XML on screen
+
+        //now your xml is loaded
+        TextView topView = findViewById(R.id.helloTextView); //must match XML id
+        String oldText = topView.getText().toString();
+        topView.setText("Java put this here");
+
+        EditText bottom = findViewById(R.id.bottomtext);
+
+        Button btn = findViewById(R.id.button);
+        btn.setText("The view was previously "+ oldText); //on startup
+        btn.setOnClickListener(  (  voidsfhguiaerhgkhersi ) ->
+        {
+                //when  you click
+           //text is empty
+            topView.setText("Edit text has " +  bottom.getText().toString());
+
+        }   ); //OnCLickListener goes in here
+
+        /*btn.setOnClickListener(new View.OnClickListener() { //anonymous class
+            @Override
+            public void onClick(View v) {
+                topView.setText("Edit text has " + bottom.getText());
+            }
+        });*/
+
     }
 }
