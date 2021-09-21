03bd1c4677131217c7b71e8bd675d7b2a26f5277
Eric Torunski <etorunski@gmail.com>
2021-09-21 15:36:23 -0400

    working on compound widgets

--- app/src/main/java/com/cst2335/torunse/MainActivity.java
+++ app/src/main/java/com/cst2335/torunse/MainActivity.java
@@ -2,6 +2,9 @@ package com.cst2335.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calls parent onCreate()
         setContentView( R.layout.activity_main ); //loads XML on screen
+
+        CheckBox cb =        findViewById(R.id.check);
+
+
+        RadioButton radio = findViewById(R.id.radio);
+        Switch sw = findViewById(R.id.sw);
+
+        sw.setOnCheckedChangeListener( ( btn, onOrOff) -> {
+            radio.setChecked(onOrOff);
+      });
+
+
+        cb.setOnCheckedChangeListener( ( b, c) -> {
+
+           if(c)
+               radio.setChecked(true);
+           else
+               radio.setChecked(false);
+        });
+
     }
 }
