d76e38128cd79398229f38f7e986358fcd6905b1
Eric Torunski <etorunski@gmail.com>
2021-09-21 15:50:04 -0400

    working on image widgets

--- app/src/main/java/com/cst2335/torunse/MainActivity.java
+++ app/src/main/java/com/cst2335/torunse/MainActivity.java
@@ -2,6 +2,7 @@ package com.cst2335.torunse;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //calls parent onCreate()
         setContentView( R.layout.activity_main ); //loads XML on screen
+
+        ImageButton btn = findViewById(R.id.button3);
+        btn.setOnClickListener( (vw) -> {
+            int width = btn.getWidth();
+            int height = vw.getHeight();
+
+        });
     }
 }
